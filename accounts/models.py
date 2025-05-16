a9e911b510d2c8d4c83940b87717a0064870e822
bemwiz <bemwzigilbert@outlook.fr>
2025-05-16 12:38:07 +0200

    Ajout du champs phone au modèle accounts. User

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='user_profiles/', null=True, blank=True, verbose_name=_('Image de profil'), help_text=_('L\'image de profil de l\'utilisateur.'))
+    phone = models.CharField(max_length=25, blank=True, null=True, verbose_name=_("Télephone"))
     email_verified = models.BooleanField(default=False)
     email_validation_token = models.CharField(max_length=64, blank=True)
 
