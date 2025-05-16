0ddc6d0dcf477cbde5226050fd0c7c69172393b5
bemwiz <bemwzigilbert@outlook.fr>
2025-05-16 15:41:18 +0200

    fake enlever les fake datas

--- terranova/urls.py
+++ terranova/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .fictif import *
+#from .fictif import *
 
 app_name = 'terranova'
 
